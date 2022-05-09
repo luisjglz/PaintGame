7679a0b3b8f333a25224c3ccb01aaf2c2f84de39
Humbert100 <93540685+Humbert100@users.noreply.github.com>
2022-05-09 12:15:53 -0500

    Update paint.py

--- paint.py
+++ paint.py
@@ -1,7 +1,7 @@
 """
 Juego: Paint
 Programador 1: Luis José González
-Programador 2: Humberto Alejandro Rosas Téllez
+Programador 2: Humberto Alejandro Rosas Téllez HART
 
 Fecha: 9 / mayo / 2022
 """
